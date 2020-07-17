c3fb965818d37ae2f7ae46a8efe3e8a9257c8e7e
daniing <36769686+daniing@users.noreply.github.com>
2020-07-16 21:51:28 -0500

    first function
    This function creates a especial "matrix", following the vector example on coursera

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,24 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## This function creates a special "matrix" 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) m <<- inverse
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+    
 }
-
-
+ 
 ## Write a short comment describing this function
-
+## This function computes the inverse of the special "matrix"
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
 }
